67296756e09eaf2d7b327bdbccaed89b72f51df4
Thaddeus  Thomas <thomasthaddeus@cityuniversity.edu>
2023-05-15 16:15:37 -0600

    MVVM
    Model View ViewModels

---
+++ MVVM/ViewModels/View1ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTemplate.ViewModels
+{
+    public class View1ViewModel
+    {
+        // Add properties and methods specific to View1
+    }
+}
---
+++ MVVM/ViewModels/View2ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTemplate.ViewModels
+{
+    public class View2ViewModel
+    {
+        // Add properties and methods specific to View2
+    }
+}
